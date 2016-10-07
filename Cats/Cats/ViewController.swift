a0f8eb25d99df878d67cb4746649c4bbb56d0aca
Nick Rigano <nrigano@Executor.local>
2016-10-07 10:53:29 -0400

    Mrowr?

--- Cats/Cats/ViewController.swift
+++ Cats/Cats/ViewController.swift
@@ -6,12 +6,45 @@
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UITableViewController {
+
+class TableViewController: UITableViewController {
+    let catList = [
+        "Norwegian",
+        "Abyssinian",
+        "Siamese",
+        "Flop-Eared",
+        "Stinky",
+        "Alley",
+        "Hairless",
+        "Puma",
+        "House",
+        "Maine Coon",
+        "Egyptian",
+        "Lying"
+    ]
     
     
-   
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return catList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "KittyCell", for: indexPath)
+        let catName = catList[indexPath.row]
+        cell.textLabel?.text = catName
+        return cell
+    }
+    
+    
+    
     
     
 }
